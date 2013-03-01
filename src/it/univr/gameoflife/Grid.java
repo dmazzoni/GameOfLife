89af49e1bc615f305cc2b17f1da54b39932409bd
Davide Mazzoni <davidemazzoni@yahoo.it>
2013-03-01 14:13:34 +0100

    Costruttore e firme metodi Grid/Cell

--- src/it/univr/gameoflife/Grid.java
+++ src/it/univr/gameoflife/Grid.java
@@ -1,14 +1,38 @@
 package it.univr.gameoflife;
 
+import java.awt.Point;
+
 public class Grid {
 	
 	Cell[][] cells;
 	
 	public Grid(int width, int height) {
-		
+		cells = new Cell[height][width];
+		for (int i = 0; i < cells.length; i++)
+			for (int j = 0; j < cells[0].length; j++)
+				cells[i][j] = new Cell(i, j);
 	}
 	
-	class Cell {
+	public boolean isAlive(int i, int j) {
 		//TODO
+		return false;
+	}
+	
+	public void changeState(int i, int j) {
+		//TODO
+	}
+	
+	public void insertShape(int i, int j, Shape shape) {
+		//TODO
+	}
+	
+	public class Cell {
+		
+		private Point coordinates;
+		private boolean alive;
+		
+		private Cell(int x, int y) {
+			coordinates = new Point(x, y);
+		}
 	}
 }
