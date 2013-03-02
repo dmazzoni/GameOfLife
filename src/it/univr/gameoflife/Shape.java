5df3bebb62379e6f3652e3a95536707e900cacdc
Davide Mazzoni <davidemazzoni@yahoo.it>
2013-03-02 13:53:13 +0100

    Shape - Celle memorizzate in array invece di set

--- src/it/univr/gameoflife/Shape.java
+++ src/it/univr/gameoflife/Shape.java
@@ -2,24 +2,38 @@ package it.univr.gameoflife;
 
 import java.awt.Point;
 import java.util.Iterator;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Shape implements Iterable<Point> {
 	
-	private final Set<Point> points;
+	private final Point[] points;
 	private final String name;
 	
 	public Shape(String name, Point... points) {
 		this.name = name;
-		this.points = new HashSet<Point>();
-		for (Point p : points)
-			this.points.add(p);
+		this.points = points;
 	}
 	
 	@Override
 	public Iterator<Point> iterator() {
-		return points.iterator();
+		return new Iterator<Point>(){
+			
+			private int i;
+			
+			@Override
+			public boolean hasNext() {
+				return i < points.length;
+			}
+			
+			@Override
+			public Point next() {
+				return points[i++];
+			}
+			
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
 	}
 
 }
