b68324ea15932fb59a83fe2ebadb0daa8e983603
Davide Mazzoni <davidemazzoni@yahoo.it>
2013-03-02 10:32:53 +0100

    Metodi Shape

--- src/it/univr/gameoflife/Shape.java
+++ src/it/univr/gameoflife/Shape.java
@@ -1,10 +1,25 @@
 package it.univr.gameoflife;
 
 import java.awt.Point;
+import java.util.Iterator;
+import java.util.HashSet;
+import java.util.Set;
 
-public abstract class Shape implements Iterable<Point> {
+public class Shape implements Iterable<Point> {
 	
-	protected Point[] points;
-	protected String nome;
+	private final Set<Point> points;
+	private final String name;
+	
+	public Shape(String name, Point... points) {
+		this.name = name;
+		this.points = new HashSet<Point>();
+		for (Point p : points)
+			this.points.add(p);
+	}
+	
+	@Override
+	public Iterator<Point> iterator() {
+		return points.iterator();
+	}
 
 }
