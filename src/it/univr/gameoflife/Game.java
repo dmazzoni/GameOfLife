9b9f36d15cb4b3b268ee4dd64f8e31d5276d7d4b
Giacomo Annaloro <giacomoannaloro92@gmail.com>
2013-03-02 11:58:27 +0100

    Struttura game

--- src/it/univr/gameoflife/Game.java
+++ src/it/univr/gameoflife/Game.java
@@ -6,56 +6,70 @@ import java.util.Set;
 
 public class Game {
 	
-	private Grid current;
-	private Grid next;
-	private Set<Point> deadCellsCoordinates;
+	private Grid grid;
+	private final Set<Point> deadCellsCoordinates;
 	private int width;
+	private int height;
 
 	public Game(int width, int height) {
-		this.current = new Grid(width, height);
+		this.grid = new Grid(width, height);
 		this.deadCellsCoordinates = new HashSet<Point>();
 		this.width = width;
+		this.height = height;
 	}
 	
 	public Grid getGrid() {
-		return this.current;
+		return this.grid;
 	}
 	
-	public void nextGeneration(int numOfThreads) {
+	private class NextGeneration {
+	
+		private Integer i;
+		private Grid current;
+		private Grid next;
+			
+		private NextGeneration (int numOfThreads) {
+			current = Game.this.grid;
+			next = new Grid(width, height);
+			
 		
-		Integer i;
+			Slave[] slaves = new Slave[numOfThreads];
+			for(int pos = 0; pos < slaves.length; pos++) {
+				slaves[pos] = new Slave();
+				slaves[pos].start();
+			}	
+			
+			for(Slave slave: slaves)
+				try {
+					slave.join();
+				} catch(InterruptedException e) {}
+			
+			current = next;
+		}
 		
-		Slave[] slaves = new Slave[numOfThreads];
-		for(int i = 0; i < slaves.length; i++) {
-			slaves[i] = new Slave();
-			slaves[i].run();
-		}	
-	}
-		
-	private class Slave extends Thread {
-
-		@Override
-		public void run() {
-			int slaveIndex;
-			while(i < LIMITE) {
-				synchronized(i) {
-					slaveIndex = i++;
+		private class Slave extends Thread {
+			
+			@Override
+			public void run() {
+				int slaveIndex;
+				while(i < height * width) {
+					synchronized(i) {
+						slaveIndex = i++;
+					}
+					Point p = new Point((int)i / width, (int) i % width);
+					if(!(deadCellsCoordinates.contains(p)))
+						checkNeighbour(p);
 				}
-				Point p = new Point((int)i / width, (int) i % width);
-				if(!(deadCellsCoordinates.contains(p)))
-					checkNeighbour(p);
+			}
+
+			private void checkNeighbour(Point p) {
+				for(int dx = -1; dx <= 1; dx++)
+					for(int dy = -1; dy <= 1; dy++)
+						if(p.x + dx > 0 && p.x + dx < XXXX && p.y + dy
+
 			}
 		}
-
-		private void checkNeighbour(Point p) {
-			for(int dx = -1; dx <= 1; dx++)
-				for(int dy = -1; dy <= 1; dy++)
-					if(p.x + dx > 0 && p.x + dx < XXXX && p.y + dy
-
-
-
-		}
+		
 	}
-	
 }
 
