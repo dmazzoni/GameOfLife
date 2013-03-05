8c11954103189f40b7c6f7002fcd418c9595c896
Giacomo Annaloro <giacomoannaloro92@gmail.com>
2013-03-05 16:59:26 +0100

    Eliminazione classe interna cella

--- src/it/univr/gameoflife/Grid.java
+++ src/it/univr/gameoflife/Grid.java
@@ -4,7 +4,7 @@ import java.awt.Point;
 
 public class Grid {
 	
-	private final Cell[][] cells;
+	private final boolean[][] cells;
 	
 	/**
 	 * Constructs and initializes a grid of the specified size. All cells are initially dead.
@@ -12,10 +12,7 @@ public class Grid {
 	 * @param height the number of rows
 	 */
 	public Grid(int width, int height) {
-		cells = new Cell[height][width];
-		for (int i = 0; i < cells.length; i++)
-			for (int j = 0; j < cells[0].length; j++)
-				cells[i][j] = new Cell(i, j);
+		cells = new boolean[height][width];
 	}
 	
 	/**
@@ -24,7 +21,7 @@ public class Grid {
 	 * @return <code>true</code> if the cell is alive, <code>false</code> otherwise.
 	 */
 	public boolean isAlive(Point p) {
-		return cells[p.x][p.y].alive;
+		return cells[p.x][p.y];
 	}
 	
 	/**
@@ -33,7 +30,7 @@ public class Grid {
 	 * @param p the point representing the coordinates of the cell.
 	 */
 	public void changeState(Point p) {
-		cells[p.x][p.y].alive = !cells[p.x][p.y].alive;
+		cells[p.x][p.y] = !cells[p.x][p.y];
 	}
 	
 	/**
@@ -47,20 +44,8 @@ public class Grid {
 			absX = point.x + shapePoint.x;
 			absY = point.y + shapePoint.y;
 			if (absX < cells.length && absY < cells[0].length)
-				cells[absX][absY].alive = true;
+				cells[absX][absY] = true;
 		}
 	}
 	
-	/**
-	 * Represents a single cell of a grid.
-	 */
-	public class Cell {
-		
-		private final Point coordinates;
-		private boolean alive;
-		
-		private Cell(int x, int y) {
-			coordinates = new Point(x, y);
-		}
-	}
 }
