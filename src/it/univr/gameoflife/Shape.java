0696b6bc5af797926a636c2c18af2acb1adaf2a3
Giacomo Annaloro <giacomoannaloro92@gmail.com>
2013-03-13 07:54:10 +0100

    Aggiunta bozza commenti javadoc per Shape

--- src/it/univr/gameoflife/Shape.java
+++ src/it/univr/gameoflife/Shape.java
@@ -3,16 +3,32 @@ package it.univr.gameoflife;
 import java.awt.Point;
 import java.util.Iterator;
 
+/**
+ * Stores a predefined shape composed by its name and its points.
+ * @author Giacomo Annaloro
+ * @author Davide Mazzoni
+ */
 public class Shape implements Iterable<Point> {
 	
 	private final Point[] points;
 	private final String name;
 	
+	/**
+	 * Constructs a shape made up of the specified points.
+	 * @param name the name of the shape
+	 * @param points the coordinates that correspond to the shape's cells in the graphic grid
+	 */
 	public Shape(String name, Point... points) {
 		this.name = name;
 		this.points = points;
 	}
 	
+	/**
+	 * Determines if the cell at the specified coordinates in this grid is alive.
+	 * @param i the row index
+	 * @param j the column index
+	 * @return The name of the shape.
+	 */
 	public String getName() {
 		return this.name;
 	}
