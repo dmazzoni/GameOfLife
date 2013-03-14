d41d0c57fc4ca7cdba2c098bfe763d47645fa916
Giacomo Annaloro <giacomoannaloro92@gmail.com>
2013-03-14 15:18:06 +0100

    Aggiunta tendina per selezione num di thread

--- src/it/univr/gameoflife/GameWindow.java
+++ src/it/univr/gameoflife/GameWindow.java
@@ -17,7 +17,7 @@ public class GameWindow extends JFrame {
 	private final GraphicGrid graphicGrid;
 	private int cellSize = 10;
 	private boolean started;
-	private final int numOfThreads = 2;
+	private int numOfThreads = 2;
 	private int delay = 200;
 	private static final Shape[] shapes = new Shape[] {
 		new Shape("Aliante", new Point(0,0), new Point(0,1), new Point(0,2), new Point(1,0), new Point(2,1)),
@@ -121,7 +121,30 @@ public class GameWindow extends JFrame {
 			}
 		});
 		toolBar.add(zoomSelector);
-
+		toolBar.addSeparator();
+		
+		final JComboBox numThreadSelector = new JComboBox(new String[]{"1", "2", "3", "4", "5"});
+		zoomSelector.setSelectedIndex(0);
+		zoomSelector.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				final int selectedIndex = numThreadSelector.getSelectedIndex();
+				synchronized (game) {
+					switch(selectedIndex) {
+					case 0: numOfThreads = 1; break;
+					case 1: numOfThreads = 2; break;
+					case 2: numOfThreads = 3; break;
+					case 3: numOfThreads = 4; break;
+					case 4: numOfThreads = 5; break;
+					default: ;
+					}
+				}
+			}
+		});
+		toolBar.add(numThreadSelector);
+		toolBar.addSeparator();
+		
 		final String[] shapeNames = new String[shapes.length + 1];
 		shapeNames[0] = "Scegli una forma e clicca sulla griglia per inserirla";
 		int i = 1;
